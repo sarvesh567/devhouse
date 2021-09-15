298ec154e6455452ae77bdca3c97be2283a8ff0e
Sarvesh Patel <sarvesh.patel@bigohtech.com>
2021-09-15 21:32:22 +0530

    initial commit

---
+++ DevHouse/DevHouseAdmin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DevHouseAdmin
+//
+//  Created by Sarvesh Patel on 15/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
